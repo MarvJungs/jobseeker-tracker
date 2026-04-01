fd4372c638d71c835d05fa9218e4128d2d13a9fa
Marvin Jung <marvinjung@outlook.com>
2026-04-01 15:48:04 +0200

    updated routing action

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::middleware('auth')->group(
                 Route::get('/dashboard', [JobapplicationController::class, 'dashboard'])->name('dashboard');
                 Route::get('/jobs', [JobapplicationController::class, 'index'])->name('jobs');
                 Route::get('/jobs/create', [JobapplicationController::class, 'create'])->name('job.create');
-                Route::get('/jobs/{job}/edit', [Jobapplication::class, 'edit'])->name('job.edit');
+                Route::get('/jobs/{job}/edit', [JobapplicationController::class, 'edit'])->name('job.edit');
                 Route::get('/jobs/import', [JobapplicationController::class, 'import'])->name('jobs.import');
                 Route::post('/jobs/import', [JobapplicationController::class, 'addJobsFromImport'])->name('jobs.addFromImport');
                 Route::post('/jobs/create', [JobapplicationController::class, 'store'])->name('job-store');
