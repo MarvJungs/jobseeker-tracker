eb9f09353f317ecf04d2b4e95903ca3ed12de4bd
Marvin Jung <marvinjung@outlook.com>
2026-05-03 22:19:49 +0200

    fixed 403 error

--- app/Policies/JobapplicationPolicy.php
+++ app/Policies/JobapplicationPolicy.php
@@ -4,7 +4,6 @@ namespace App\Policies;
 
 use App\Models\Jobapplication;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class JobapplicationPolicy
 {
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,11 @@ Route::middleware('auth')->group(
             function () {
                 Route::get('/dashboard', [JobapplicationController::class, 'dashboard'])->name('dashboard');
                 Route::get('/jobs', [JobapplicationController::class, 'index'])->name('jobs');
-                Route::get('/jobs/create', [JobapplicationController::class, 'create'])->name('job.create')->can('create');
+                Route::get('/jobs/create', [JobapplicationController::class, 'create'])->name('job.create')->can('create', \App\Models\Jobapplication::class);
                 Route::get('/jobs/{job}/edit', [JobapplicationController::class, 'edit'])->name('job.edit')->can('update', 'job');
                 Route::get('/jobs/import', [JobapplicationController::class, 'import'])->name('jobs.import');
                 Route::post('/jobs/import', [JobapplicationController::class, 'addJobsFromImport'])->name('jobs.addFromImport');
-                Route::post('/jobs/create', [JobapplicationController::class, 'store'])->name('job.store')->can('create');
+                Route::post('/jobs/create', [JobapplicationController::class, 'store'])->name('job.store')->can('create', \App\Models\Jobapplication::class);
                 Route::post('/jobs/{job}/edit', [JobapplicationController::class, 'update'])->name('job.update')->can('update', 'job');
                 Route::delete('/jobs/{job}/delete', [JobapplicationController::class, 'destroy'])->name('job.destroy')->can('delete', 'job');
             }
